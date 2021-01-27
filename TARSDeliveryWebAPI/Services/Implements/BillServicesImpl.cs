7d451392cc1b9af07d8c2f1a44f21792b244a04e
Kitajima2910 <kitajima2910@gmail.com>
2021-01-27 19:20:32 +0700

    hoaipx/20210127-02: Added: - BillsController, BillPackage, Index, fonts, script, common Modified: - BillServicesImpl, IBillServices, _Layout Deleted: - BillController

--- TARSDeliveryWebAPI/Services/Implements/BillServicesImpl.cs
+++ TARSDeliveryWebAPI/Services/Implements/BillServicesImpl.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -22,6 +23,43 @@ namespace TARSDeliveryWebAPI.Services.Implements
             await context.GetBills.AddAsync(bill);
             var created = await context.SaveChangesAsync();
             return created > 0;
+        }
+
+        public async Task<IEnumerable<BillPackage>> GetBillPackages()
+        {
+            return await context.GetBills
+                .Join(context.GetPackages,
+                    b => b.PackageId,
+                    p => p.Id,
+                    (b, p) => new BillPackage
+                    {
+                        BillId = b.Id, // get
+                        BillAccountId = b.AccountId,
+                        BillPackageId = b.PackageId,
+                        BillCreate_at = b.Create_at, // get
+                        BillUpdate_at = b.Update_at,
+                        BillDelete_at = b.Delete_at,
+
+                        PackageId = p.Id,
+                        PackageTitle = p.Title, // get
+                        PackageNameFrom = p.NameFrom, // get
+                        PackageEmail = p.Email, // get
+                        PackageAddressFrom = p.AddressFrom,
+                        PackageType = p.Type, // get
+                        PackageZipCode = p.ZipCode,
+                        PackageNameTo = p.NameTo,
+                        PackageAddressTo = p.AddressTo,
+                        PackageWeight = p.Weight,
+                        PackageDistance = p.Distance,
+                        PackageMessage = p.Message,
+                        PackageTotalPrice = p.TotalPrice, // get
+                        PackageStatus = p.Status, // get
+                        PackageBranchId = p.BranchId,
+                        PackageAccountId = p.AccountId,
+                        PackageCreate_at = p.Create_at,
+                        PackageUpdate_at = p.Update_at,
+                        PackageDelete_at = p.Delete_at,
+                    }).Where(m => m.BillDelete_at == null && m.PackageDelete_at == null).ToListAsync();
         }
     }
 }
