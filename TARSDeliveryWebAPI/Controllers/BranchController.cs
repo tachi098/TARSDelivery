25737700839f897feade212765586ca87ee0d036
ngotrungkien98 <ngotrungkien4798@gmail.com>
2021-01-28 09:56:52 +0700

    Branch List- Branch Create

---
+++ TARSDeliveryWebAPI/Controllers/BranchController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TARSDeliveryWebAPI.Models;
+using TARSDeliveryWebAPI.Services.Interfaces;
+namespace TARSDeliveryWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BranchController : ControllerBase
+    {
+        private IBranchServices branchServices;
+        public BranchController(IBranchServices branchServices)
+        {
+            this.branchServices = branchServices;
+        }
+        public async Task<IActionResult> GetBranches()
+        {
+
+            return Ok(await branchServices.GetBranches());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(Branch branch)
+        {
+            return Ok(await branchServices.PostBranch(branch));
+        }
+    }
+}
---
+++ TARSDeliveryWebApp/Areas/Admin/Controllers/BranchController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using TARSDeliveryWebApp.Models;
+
+namespace TARSDeliveryWebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BranchController : Controller
+    {
+        private const string uriBranch = "http://localhost:50354/api/Branch/";
+        private const string uriRole = "http://localhost:50354/api/Role/";
+        private HttpClient httpClient = new HttpClient();
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Index()
+        {
+            var model = JsonConvert.DeserializeObject<IEnumerable<Branch>>(httpClient.GetStringAsync(uriBranch).Result);
+            return View(model);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Branch branch)
+        {
+            var model = httpClient.PostAsJsonAsync(uriBranch, branch).Result;
+            if (model.IsSuccessStatusCode)
+            {
+                return RedirectToAction("index", "Branch");
+            }
+            return View();
+        }
+    }
+}
